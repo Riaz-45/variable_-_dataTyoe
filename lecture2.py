1a36acc1339eee3ef26a9662509f7f72d85ea66d
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-03-18 01:21:24 +0600

    string concatanation and string eln() function

---
+++ lecture2.py
@@ -0,0 +1,44 @@
+#Valid String in Python
+
+"""
+
+str1 = "This is a string." #more appropriate
+str2 = 'This is a string.\n' #\n" new line create kore
+print(str1, str2, str3)
+
+"""
+#str3 = """This is a string."""
+
+
+#string concatenation
+
+"""
+
+str1 = "hello"
+str2 = "world"
+str4 = str1 + str2
+print(str4)
+
+"""
+
+#string length "len()"
+
+"""
+
+str1 = "Legendary"
+len1 = len(str1)
+print(len1)
+
+str2 = "Ninja"
+len2 = len(str2)
+print(len2)
+
+str3 = "Alex"
+len3 = len(str3)
+print(len3)
+
+final_str = str1 + " " + str2 + " " + str3 #" " free space also be calculated in string length
+print(final_str)
+print(len(final_str))
+
+"""
